29d8e1bfc036cc1ff46a557bbe290eedacf49318
Timothy Wilson <tiwilson@my.bellevue.edu>
2019-03-29 23:09:46 -0500

    Adding presentations

--- nodequiz/src/app/session/qzselection/qzselection.component.ts
+++ nodequiz/src/app/session/qzselection/qzselection.component.ts
@@ -21,5 +21,21 @@ export class QzselectionComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    var coll = document.getElementsByClassName("openVideo");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
   }
 }
+
